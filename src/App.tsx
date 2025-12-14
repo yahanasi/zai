df61926841674047d55dfb122087a4096fee0083
yahanasi <149941167+yahanasi@users.noreply.github.com>
2025-12-14 16:31:52 +0900

    Change audio source format from flac to ogg

--- src/App.tsx
+++ src/App.tsx
@@ -54,7 +54,7 @@ const BgmControl = () => {
   const [autoTried, setAutoTried] = useState(false);
 
   // 相对路径，两边都能用
-  const src = "bgm.flac";
+  const src = "bgm.ogg";
 
   // 第一次挂载时，尝试自动播放
   useEffect(() => {
